936e6d7c2ec887e8e73da1cffe3bb7b4acc3db28
Seth Mazer <116219473+SethMazer@users.noreply.github.com>
2022-11-08 19:12:58 -0500

    inclosed if statements in function
    Inclosed if statements in function to restart script after every input

--- bartsimpson.py
+++ bartsimpson.py
@@ -1,13 +1,18 @@
 
 
-
-# Sets speech to what user input
-speech = input("Talk to Bart Simpson: ")
-
-#If users says skateboard print 'Cowabunga!' else if user apologizes print "Don't have a cow man!" else print 'Eat my shorts!
-if speech in ['Skateboard', 'skateboard', 'Skate Board', 'skate board']:
+def bartsimpson():
+    #Sets Speech to user input
+    speech = input('Talk to Bart Simpson: ')
+    #if user says Skateboard, print 'Cowabunga!'
+    if speech in ['Skateboard', 'skateboard', 'Skate Board', 'skate board']:
       print('Cowabunga!')
-elif speech in ['Sorry', 'sorry', 'My Apologies', 'my apologies', 'Apologies', 'apologies']:
-    print("Don't have a cow man!")
-else: print('Eat my shorts!')
+    #if user says Sorry, print "Don't have a cow man!"  
+    elif speech in ['Sorry', 'sorry', 'My Apologies', 'my apologies', 'Apologies', 'apologies']:
+        print("Don't have a cow man!")
+    #If user says something else, print 'Eat my shorts!' 
+    else: print('Eat my shorts!')
 
+#Restarts script after input
+while True:
+    bartsimpson()
+    
